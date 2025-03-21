4852ecbe5e6e215867bd891604767224939cc2dc
Jeffeson Ribeiro de Brito <Jeffeson9898056@edu.df.senac.br>
2025-03-21 11:56:40 -0300

    Adcionei as configurações de Gerar acesso medico

--- exames/urls.py
+++ exames/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("cancelar_pedido/<int:pedido_id>", views.cancelar_pedido, name="cancelar_pedido"),
     path('gerenciar_exames/', views.gerenciar_exames, name="gerenciar_exames"),
     path('solicitar_senha_exame/<int:exame_id>', views.solicitar_senha_exame, name="solicitar_senha_exame"),
+    path('gerar_acesso_medico/', views.gerar_acesso_medico, name="gerar_acesso_medico"),
+    path('acesso_medico/<str:token>', views.acesso_medico, name="acesso_medico"),
+    
 ]
